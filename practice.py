86f82d8f700a243d4b49b25f475d7efee29964c1
kaps <kasperjonassen@yahoo.dk>
2026-02-22 12:33:53 +0100

    added

---
+++ practice.py
@@ -0,0 +1,87 @@
+#name = input("What is your name? ")
+#age = input("What is your age? ")
+
+#print(f"Hello {name}, you are {age} old")
+
+'''
+number = int(input("Whats your number? "))
+
+if number > 0:
+	print("Number positiv")
+elif number < 0:
+	print("Number negativ")
+else:
+	print("number zero")
+	'''
+
+#for i in range(21):
+#	print(i)
+'''
+num = 0
+while num < 20:
+	num += 1
+	print(num)
+'''
+'''
+fruits = ['banana', 'apple', 'orange', 'kiwi']
+
+print(fruits)
+print("adding one: ")
+fruits.append('pear')
+print(fruits)
+print("Removing the first fruit")
+fruits.pop(0)
+print(fruits)
+print("fucking sejt")
+'''
+
+'''
+cities = ("copenhagen", "aarhus", "odense")
+
+print(cities)
+#cities[0] = "new city" cant to this maaaan
+print(f"second city: {cities[1]}")
+'''
+'''
+sick = {"navn": "Kasper", "alder": 25, "by": "København"}
+sick["swag"] = "top"
+print(sick)
+print(f"lets get the by: {sick["by"]} ")
+'''
+'''
+l = int(input("length? "))
+b = int(input("bredte? "))
+
+r = l * b
+
+print(r)
+'''
+'''
+numbers = list(range(0, 70))
+
+for i in numbers:
+	if i > 10:
+		print(i)
+'''
+
+'''
+
+string = input("STRING ?!?!?!: ")
+
+print(string[::-1])
+'''
+
+'''
+def func(*args):
+	return sum(args)
+
+print(func(1,2,3,4,5))
+'''
+
+'''
+def func(**kwargs):
+	for key, value in kwargs.items():
+		print(f"{key}: {value}")
+
+func(name="Bent", age=23, town="swagcity")
+'''
